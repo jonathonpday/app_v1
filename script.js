594c5df39433e75836e72b4554566f8328819726
Jonathon Day <jonathonpday@me.com>
2021-12-02 19:58:50 +0000

    first version with sound and text

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const buttons = document.querySelectorAll('[data-carousel-button]')
+const nameBox = document.querySelector('.name')
+
+let audioContext = new AudioContext()
+
+function sound() {
+  let osc = audioContext.createOscillator()
+  osc.type = 'sine'
+  osc.start(audioContext.currentTime + 10)
+  osc.stop(audioContext.currentTime + 11)
+  osc.connect(audioContext.destination)
+}
 
 buttons.forEach(button => {
   button.addEventListener('click', () => {
+    nameBox.innerText = ''
     const offset = button.dataset.carouselButton === 'next' ? 1 : -1
     const slides = button
       .closest('[data-carousel]')
@@ -14,5 +26,16 @@ buttons.forEach(button => {
 
     slides.children[newIndex].dataset.active = true
     delete activeSlide.dataset.active
+
+    if (audioContext.state === 'suspended') {
+      audioContext.resume()
+    }
+
+    sound()
+
+    const nameText = slides.children[newIndex].children[0].id
+    setTimeout(() => {
+      nameBox.innerText = nameText
+    }, 10000)
   })
 })
